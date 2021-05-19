d97a5735f8fcd9241855a7970d8b48a4fdcd6a52
Donald Smith <donaldsmith1911@gmail.com>
2021-05-19 09:14:23 -0500

    upodates

---
+++ Bootstrap/app.js
@@ -0,0 +1,13 @@
+console.log('Hello World');
+var sum = add(2, 87895);
+console.log(sum);
+
+function add(num1, num2) {
+    console.log('In function');
+    return num1 + num2;
+}
+
+function sayHello(){
+    console.log('In Function!');
+}
+sayHello();
